c1c6423548becd36969a9aa4514fdaf4324e538c
BANice <suhangyang@gmail.com>
2018-03-14 15:15:45 +0800

    创建ws

---
+++ start.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suhangyang
+ * Date: 2018/3/14
+ * Time: 下午3:13
+ */
+
+use Workerman\Worker;
+
+require_once __DIR__.'/Autoloader.php';
+
+$wxWorker = new  Worker('ws://0.0.0.0:8383');
+
+$wxWorker->count =1;
+
+$wxWorker->onMessage =function ($connection,$data)
+{
+
+    $connection->send($data);
+
+};
+
+Worker::runAll();
